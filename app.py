e98287715a377427c55293fd569bea2faf2e1817
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-06-02 20:07:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():
-    return 'GreyMatters'
-if __name__ == "__main__":
-    app.run()
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Run the Flask app on port 80
+    app.run(host='0.0.0.0', port=80)
